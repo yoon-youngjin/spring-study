0def3e49931f2eb0b0702531ece603826750034e
youngjin yoon <dudwls143@naver.com>
2022-03-02 19:13:45 +0900

    2022/03/02

--- practice/5주차/mybatis-demo/src/main/java/dev/yoon/mybatisdemo/TestComponent.java
+++ practice/5주차/mybatis-demo/src/main/java/dev/yoon/mybatisdemo/TestComponent.java
@@ -24,23 +24,22 @@ public class TestComponent {
         BoardDto boardDto = new BoardDto();
         boardDto.setName("New Board");
         boardDao.createBoard(boardDto);
-        System.out.println(boardDto.getId());
-//        PostDto newPost = new PostDto();
-//        newPost.setTitle("From Mybatis");
-//        newPost.setContent("Hello Database!");
-//        newPost.setWriter("yoon");
-//        newPost.setBoard(1);
-//
-//        this.postDao.createPost(newPost);
-//
-//        List<PostDto> postDtoList = this.postDao.readPostAll();
-//        System.out.println(postDtoList.get(postDtoList.size() - 1));
-//
-//        PostDto postDto = postDtoList.get(0);
-//        postDto.setContent("Update From Mybatis");
-//        postDao.updatePost(postDto);
-//
-//        System.out.println(this.postDao.readPost(postDto.getId()));
+        PostDto newPost = new PostDto();
+        newPost.setTitle("From Mybatis");
+        newPost.setContent("Hello Database!");
+        newPost.setWriter("yoon");
+        newPost.setBoard(1);
+
+        this.postDao.createPost(newPost);
+
+        List<PostDto> postDtoList = this.postDao.readPostAll();
+        System.out.println(postDtoList.get(postDtoList.size() - 1));
+
+        PostDto postDto = postDtoList.get(0);
+        postDto.setContent("Update From Mybatis");
+        postDao.updatePost(postDto);
+
+        System.out.println(this.postDao.readPost(postDto.getId()));
 
 
     }
