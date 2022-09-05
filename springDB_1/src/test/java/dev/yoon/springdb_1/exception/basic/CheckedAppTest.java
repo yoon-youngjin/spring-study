36be8d08af42b9a945171ffd5ae648f0fd68eec1
yoon <dudwls143@gmail.com>
2022-09-05 20:22:51 +0900

    Section6 자바 예외 이해 실습 완료

---
+++ springDB_1/src/test/java/dev/yoon/springdb_1/exception/basic/CheckedAppTest.java
@@ -0,0 +1,48 @@
+package dev.yoon.springdb_1.exception.basic;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+public class CheckedAppTest {
+
+
+    @Test
+    void checked() {
+        Controller controller = new Controller();
+        Assertions.assertThatThrownBy(controller::request)
+                .isInstanceOf(Exception.class);
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request() throws SQLException, ConnectException {
+            service.logic();
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic() throws SQLException, ConnectException {
+            repository.call();
+            networkClient.call();
+        }
+    }
+    static class NetworkClient {
+        public void call() throws ConnectException {
+            throw new ConnectException("연결 실패"); // 체크 예외
+        }
+    }
+    static class Repository {
+        public void call() throws SQLException {
+            throw new SQLException("ex"); // 체크 예외
+        }
+    }
+}
---
+++ springDB_1/src/test/java/dev/yoon/springdb_1/exception/basic/CheckedTest.java
@@ -0,0 +1,72 @@
+package dev.yoon.springdb_1.exception.basic;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CheckedTest {
+
+    static Logger log = LoggerFactory.getLogger(CheckedTest.class);
+
+    @Test
+    void checked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void checked_throw() {
+        Service service = new Service();
+        Assertions.assertThatThrownBy(service::callThrow)
+                .isInstanceOf(MyCheckedException.class);
+    }
+
+    /**
+     * 컴파일러가 체크하는 체크 예외 생성
+     * Exception을 상속받은 예외는 체크 예외가 된다.
+     */
+    static class MyCheckedException extends Exception {
+        public MyCheckedException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * Checked 예외
+     * 예외를 잡아서 처리하거나, 던지거나 둘중 하나를 필수로 선택해야 한다.
+     */
+    static class Service {
+        Repository repository = new Repository();
+        /**
+         * 예외를 잡아서 처리하는 코드
+         */
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyCheckedException e) {
+                // 예외 처리 로직
+                // exception의 stack trace를 출력할 때는 마지막 파라미터에 exception만 넣어주면 된다.
+                log.info("예외 처리, message={}", e.getMessage(), e);
+            }
+        }
+
+        /**
+         * 체크 예외를 밖으로 던지는 코드
+         * 체크 예외는 예외를 잡지 않고 밖으로 던지려면 throws 예외를 메서드에 필수로 선언해야한다.
+         * @throws MyCheckedException
+         */
+        public void callThrow() throws MyCheckedException {
+            repository.call();
+        }
+
+    }
+
+    static class Repository {
+        public void call() throws MyCheckedException {
+            throw new MyCheckedException("ex");
+        }
+    }
+
+}
