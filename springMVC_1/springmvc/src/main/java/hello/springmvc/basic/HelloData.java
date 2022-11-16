5e89d0173433868286eb0ef1f3daf430772b98c1
yoon <dudwls143@gmail.com>
2022-11-16 23:08:41 +0900

    Section6: 스프링 MVC - 기본 기능 실습 완료

--- springMVC_1/springmvc/src/main/java/hello/springmvc/basic/HelloData.java
+++ springMVC_1/springmvc/src/main/java/hello/springmvc/basic/HelloData.java
@@ -1,8 +1,12 @@
 package hello.springmvc.basic;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class HelloData {
     private String username;
     private int age;
