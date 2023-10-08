1977b3ca4ef4a6b39f047b76714aaaddb0cea0c8
dudwls143 <dudwls143@toss.im>
2023-10-08 15:24:36 +0900

    Java/Spring 테스트를 추가하고 싶은 개발자들의 오답노트 헥사고날 정리

--- developer's-note-of-incorrect-answer-for-adding-tests/kotlin-test-code-with-architecture/src/main/kotlin/com/example/kotlintestcodewitharchitecture/user/controller/UserController.kt
+++ developer's-note-of-incorrect-answer-for-adding-tests/kotlin-test-code-with-architecture/src/main/kotlin/com/example/kotlintestcodewitharchitecture/user/controller/UserController.kt
@@ -1,17 +1,12 @@
 package com.example.kotlintestcodewitharchitecture.user.controller
 
 import com.example.kotlintestcodewitharchitecture.user.controller.port.UserService
-import com.example.kotlintestcodewitharchitecture.user.controller.request.UserUpdateRequest
-import com.example.kotlintestcodewitharchitecture.user.controller.response.MyProfileResponse
 import com.example.kotlintestcodewitharchitecture.user.controller.response.UserResponse
 import java.net.URI
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
